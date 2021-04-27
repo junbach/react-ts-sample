a53bb6951e08fee5c948490afe4c3c358688d384
minh.b <minh.b@kbtg.tech>
2021-04-27 17:50:40 +0700

    feat: add antd ui library

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
 import "./App.css";
 
+import { SendOutlined } from "@ant-design/icons";
+import { Button, Divider, Layout, notification } from "antd";
 import { ReactElement } from "react";
 
 import logo from "./logo.svg";
 
+const { Header, Footer, Content } = Layout;
+const sendMessage = () =>
+  notification.info({
+    message: "Hello, world!",
+    description: "This is a greeting message sent from the moon",
+  });
+
 export default function App(): ReactElement {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Header className="appHeader">
+        <h1>Website Title</h1>
+      </Header>
+      <Content className="appContent">
+        <img src={logo} className="appLogo" alt="logo" />
+        <Divider>Main Content</Divider>
+        <Button onClick={sendMessage} icon={<SendOutlined />}>
+          Send me a message
+        </Button>
+      </Content>
+      <Footer className="appFooter">Footer</Footer>
+    </Layout>
   );
 }
